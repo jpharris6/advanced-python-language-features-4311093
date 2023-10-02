ee7ec7718bc59a2e1afbad1f5a0743e12b67fdc6
jpharris6 <80963052+jpharris6@users.noreply.github.com>
2023-10-02 15:10:43 +0000

    Updated some of the chapter 5 files

--- Start/Ch_5/capture_start.py
+++ Start/Ch_5/capture_start.py
@@ -6,3 +6,10 @@ name = input("What is your name? ")
 match name:
     case "":
         print("Hello, anonymous!")
+    case "Joe" | "Joseph" as s:
+        print(f"Oh, hi there {s}!")
+    case "Justin" | "Brandy" as a:
+        print(f"Welcome {a}!!!!!")
+    case name:
+        print(f"Hello, {name}!")
+    
